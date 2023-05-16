98d13a11f28efa6cafef7c0c5365d11ab0b04f4b
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-05-16 15:09:11 +0600

    validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,41 +8,19 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function showCorrectHomepage()
-    {
-        if (auth()->check()) {
-            return view('homepage-feed');
-        } else {
-            return view('homepage');
-        }
-    }
-
-    public function login(Request $request)
-    {
-        $incomingFields = $request->validate([
-            'loginusername' => 'required',
-            'loginpassword' => 'required'
-        ]);
-
-        if (auth()->attempt(['username' => $incomingFields['loginusername'], 'password' => $incomingFields['loginpassword']])) {
-            $request->session()->regenerate();
-            return 'Congrats!!!';
-        } else {
-            return 'Sorry';
-        }
-    }
-
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
             'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
-            'email' => ['required', Rule::unique('users', 'email')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
             'password' => ['required', 'min:8', 'confirmed']
+
         ]);
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
         User::create($incomingFields);
-        return 'Hello from register function';
+
+        return "Hi from register function";
     }
 }
